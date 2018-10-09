d27488c8d8f79055a896b87d83d4b801ad7a80b6
rofrapi@gmail.com <rofrapi@gmail.com>
2018-10-09 21:46:53 +0200

    Add files to Application.Seedwork

---
+++ Application.Seedwork/ProjectionsExtensionMethods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Seedwork;
+using Infrastructure.Crosscutting.Adapter;
+
+namespace Application.Seedwork
+{
+    public static class ProjectionsExtensionMethods
+    {
+        /// <summary>
+        /// Project a type using a DTO
+        /// </summary>
+        /// <typeparam name="TProjection">The dto projection</typeparam>
+        /// <param name="item">The source entity to project</param>
+        /// <returns>The projected type</returns>
+        public static TProjection ProjectedAs<TProjection>(this Entity item)
+            where TProjection : class, new()
+        {
+            var adapter = TypeAdapterFactory.CreateAdapter();
+            return adapter.Adapt<TProjection>(item);
+        }
+
+        /// <summary>
+        /// projected a enumerable collection of items
+        /// </summary>
+        /// <typeparam name="TProjection">The dtop projection type</typeparam>
+        /// <param name="items">the collection of entity items</param>
+        /// <returns>Projected collection</returns>
+        public static List<TProjection> ProjectedAsCollection<TProjection>(this IEnumerable<Entity> items)
+            where TProjection : class, new()
+        {
+            var adapter = TypeAdapterFactory.CreateAdapter();
+            return adapter.Adapt<List<TProjection>>(items);
+        }
+    }
+}
---
+++ Application.Seedwork/ValidationExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Crosscutting.Validator;
+
+namespace Application.Seedwork
+{
+    public static class ValidationExtensionMethods
+    {
+        /// <summary>
+        /// Validate Entity calling IValidatableObject.Validate
+        /// </summary>
+        /// <param name="item">Entity to validate</param>
+        public static void Validate<TEntity>(this TEntity item)
+            where TEntity : class, new()
+        {
+            //Recover validator
+            var validator = EntityValidatorFactory.CreateValidator();
+
+            //Validate entity
+            if (!validator.IsValid(item))
+                throw new ApplicationValidationErrorsException(validator.GetInvalidMessages(item));
+        }
+    }
+}
