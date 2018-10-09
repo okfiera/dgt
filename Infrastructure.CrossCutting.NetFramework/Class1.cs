172c94f03efffc100171e51164a7280c7bca7710
rofrapi@gmail.com <rofrapi@gmail.com>
2018-10-09 12:46:38 +0200

    First commit

---
+++ Infrastructure.CrossCutting.NetFramework/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infrastructure.CrossCutting.NetFramework
+{
+    public class Class1
+    {
+    }
+}
